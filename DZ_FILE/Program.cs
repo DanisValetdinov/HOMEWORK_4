e1bccce01d67f90ac917e47ba0e50d4fad025485
DanisValetdinov <@DESKTOP-GMU4C52.(none)>
2024-11-21 23:15:23 +0300

    Добавьте файлы проекта.

---
+++ DZ_FILE/Program.cs
@@ -0,0 +1,194 @@
+﻿namespace std
+{
+    class DZ_FILE
+    {
+        static void Main()
+        {
+            TASK1();
+            TASK2();
+            TASK3();
+            TASK4();
+        }
+        static void TASK1()
+        {
+            /*1. Вывести на экран массив из 20 случайных чисел. Ввести два числа из этого массива,
+            которые нужно поменять местами. Вывести на экран получившийся массив.*/
+            Console.WriteLine("Задание 1. Создать массив со случайными числами, поменять два числа местами и вывести массив на экран");
+            Random Rand_Num = new Random();
+            int[] IntAr = new int[20];
+            for (int i = 0; i < IntAr.Length; i++)
+            {
+                IntAr[i] = Rand_Num.Next(1, 100);
+            }
+            foreach (int i in IntAr)
+            {
+                Console.Write($"{i}, ");
+            }
+            int Swap = IntAr[4];
+            IntAr[4] = IntAr[9];
+            IntAr[9] = Swap;
+            foreach (int i in IntAr)
+            {
+                Console.Write($"{i}, ");
+            }
+        }
+        static void TASK2()
+        {
+            /*2. Написать метод, где в качества аргумента будет передан массив (ключевое слово
+            params). Вывести сумму элементов массива (вернуть). Вывести (ref) произведение
+            массива. Вывести (out) среднее арифметическое в массиве.*/
+            Console.WriteLine("Задание 2. Написать метод, куда будет передан массив. Вычислить сумму, произведение и среднее арифметическое элементов в массиве");
+            int Multi = 1;
+            double Average;
+            int[] Array = { 11, 23, 45, 67, 89, 13, 14, 15, 16, 17, 18, 32 };
+            int Sum = Array1(ref Multi, out Average, Array);
+            Console.WriteLine($"Сумма элементов массива - {Sum}\nПроизведение элементов массива - {Multi}\nСреднее арифметическое элементов массива - {Average}");
+        }
+        static void TASK3()
+        {
+            /*3. Пользователь вводит числа. Если числа от 0 до 9, то необходимо в консоли нарисовать
+            изображение цифры в виде символа #.Если число больше 9 или меньше 0, то консоль
+            должна окраситься в красный цвет на 3 секунды и вывести сообщение об ошибке. Если
+            пользователь ввёл не цифру, то программа должна выпасть в исключение. Программа
+            завершает работу, если пользователь введёт слово: exit или закрыть (оба варианта
+            должны сработать) - консоль закроется.*/
+            Console.WriteLine("Задание 3. Нарисовать в консоли изображение цифры в виде символа #");
+            bool Flag = true;
+            while (Flag)
+            {
+                Console.Write("Введите цифру(от 0 до 9):");
+                string String = Console.ReadLine();
+                switch (String)
+                {
+                    case "0":
+                        Console.WriteLine("####\n#  #\n#  #\n#  #\n#  #\n####");
+                        break;
+                    case "1":
+                        Console.WriteLine("  ##\n ###\n# ##\n  ##\n  ##");
+                        break;
+                    case "2":
+                        Console.WriteLine("####\n#  #\n ###\n##  \n####");
+                        break;
+                    case "3":
+                        Console.WriteLine("####\n  ##\n ###\n  ##\n####");
+                        break;
+                    case "4":
+                        Console.WriteLine("## ##\n## ##\n#####\n   ##\n   ##");
+                        break;
+                    case "5":
+                        Console.WriteLine("####\n##  \n####\n  ##\n####");
+                        break;
+                    case "6":
+                        Console.WriteLine("#####\n##  \n#####\n## ##\n#####");
+                        break;
+                    case "7":
+                        Console.WriteLine("####\n  ##\n ###\n  ##\n  ##");
+                        break;
+                    case "8":
+                        Console.WriteLine("#####\n## ##\n#####\n## ##\n#####");
+                        break;
+                    case "9":
+                        Console.WriteLine("#####\n## ##\n#####\n   ##\n#####");
+                        break;
+                    case "exit":
+                        Flag = false;
+                        break;
+                    case "закрыть":
+                        Flag = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Clear();
+                        Thread.Sleep(3000);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Clear();
+                        Console.WriteLine("Ошибка");
+                        break;
+                }
+            }
+        }
+        static void TASK4()
+        {
+            /*4. Создать структуру Дед. У деда есть имя, уровень ворчливости (перечисление), массив
+            фраз для ворчания (прим.: “Проститутки!”, “Гады!”), количество синяков от ударов
+            бабки = 0 по умолчанию. Создать 5 дедов. У каждого деда - разное количество фраз для
+            ворчания. Напишите метод (внутри структуры), который на вход принимает деда,
+            список матерных слов (params). Если дед содержит в своей лексике матерные слова из
+            списка, то бабка ставит фингал за каждое слово. Вернуть количество фингалов.*/
+            Console.WriteLine("Задание 4. Создать дедов и узнать сколько они получат синяков");
+            Deds Ded1 = new Deds();
+            Ded1.Name = "Юрий";
+            Ded1.Grumb = TierOfGrumb.Medium;
+            Ded1.Phrase = new string[] { "Дебил", "Придурок", "Паскуда", "Наркоман" };
+            Deds Ded2 = new Deds();
+            Ded2.Name = "Владимир";
+            Ded2.Grumb = TierOfGrumb.High;
+            Ded2.Phrase = new string[] { "Недоношенный", "Мусор", "Как тебя земля носит", "Гад", "Проститутка" };
+            Deds Ded3 = new Deds();
+            Ded3.Name = "Прохор";
+            Ded3.Grumb = TierOfGrumb.Medium;
+            Ded3.Phrase = new string[] { "Придурок", "Дебилоид", "Проститутка", "Выпердыш", "Дегроид" };
+            Deds Ded4 = new Deds();
+            Ded4.Name = "Всеволод";
+            Ded4.Grumb = TierOfGrumb.SSS;
+            Ded4.Phrase = new string[] { "Гад", "Тварь", "Недостойный жить", "Сталина на тебя не хватает", "Наркоман", "Проститутка", "Я твою маму в кино водил"};
+            Deds Ded5 = new Deds();
+            Ded5.Name = "Анатолий";
+            Ded5.Grumb = TierOfGrumb.Low;
+            Ded5.Phrase = new string[] { "Дурак" };
+
+            string[] CheckArray = { "Гад", "Тварь", "Недостойный жить", "Сталина на тебя не хватает", "Наркоман", "Проститутка", "Я твою маму в кино водил" };
+            Ded1.Count = Ded1.Injury(Ded1, CheckArray);
+            Ded2.Count = Ded2.Injury(Ded2, CheckArray);
+            Ded3.Count = Ded3.Injury(Ded3, CheckArray);
+            Ded4.Count = Ded4.Injury(Ded4, CheckArray);
+            Ded5.Count = Ded5.Injury(Ded5, CheckArray);
+
+            Console.WriteLine($"{Ded1.Name} получил синяков: {Ded1.Count}\n" +
+                $"{Ded2.Name} получил синяков: {Ded2.Count}\n" +
+                $"{Ded3.Name} получил синяков: {Ded3.Count}\n" +
+                $"{Ded4.Name} получил синяков: {Ded4.Count}\n" +
+                $"{Ded5.Name} получил синяков: {Ded5.Count}\n");
+        }
+        static int Array1(ref int Multi, out double Average, params int[] MyArray)
+        {
+            int Sum = 0;
+            foreach (int i in MyArray)
+            {
+                Sum += i;
+                Multi *= i;
+            }
+            Average = Sum / MyArray.Length;
+            return Sum;
+        }
+        enum TierOfGrumb
+        {
+            Low,
+            Medium,
+            High,
+            SSS
+        }
+
+        struct Deds
+        {
+            public string Name;
+            public TierOfGrumb Grumb;
+            public string[] Phrase;
+            public uint Count;
+            public uint Injury(Deds Ded, params string[] array)
+            {
+                uint Count = 0;
+                foreach (string s in Ded.Phrase)
+                {
+                    if (array.Contains(s))
+                    {
+                        Count++;
+                    }
+                }
+                return Count;
+            }
+        }
+
+    }
+}
---
+++ DZ_TUMAKOV/Program.cs
@@ -0,0 +1,229 @@
+﻿namespace std
+{
+    class DZ_TUMAKOV
+    {
+        static void Main()
+        {
+            TASK1();
+            TASK2();
+            TASK3();
+            TASK4();
+            TASK5();
+            TASK6();
+        }
+        static void TASK1()
+        {
+            /*/*Упражнение 5.1 Написать метод, возвращающий наибольшее из двух чисел. Входные
+            параметры метода – два целых числа. Протестировать метод.*/
+            Console.WriteLine("Упражнение 5.1. Создать и протестировать метод, возвращающий наибольшее из двух чисел");
+            Console.Write("Введите первое число: ");
+            bool IsNum = int.TryParse(Console.ReadLine(), out int a);
+            Console.Write("Введите второе число: ");
+            bool IsNum2 = int.TryParse(Console.ReadLine(), out int b);
+            if (IsNum && IsNum2)
+            {
+                Maximum(a, b);
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не числа");
+            }
+        }
+        static void TASK2()
+        {
+            /*Упражнение 5.2 Написать метод, который меняет местами значения двух передаваемых
+            параметров. Параметры передавать по ссылке. Протестировать метод.*/
+            Console.WriteLine("Упражнение 5.2. Поменять местами значения двух переменных в методе");
+            string str1 = "вет";
+            string str2 = "При";
+            Swap(ref str1, ref str2);
+            Console.WriteLine($"Первая строка - {str1}, Вторая строка - {str2}");
+        }
+        static void TASK3()
+        {
+            /*Упражнение 5.3 Написать метод вычисления факториала числа, результат вычислений
+            передавать в выходном параметре. Если метод отработал успешно, то вернуть значение true;
+            если в процессе вычисления возникло переполнение, то вернуть значение false. Для
+            отслеживания переполнения значения использовать блок checked.*/
+            Console.WriteLine("Уражнение 5.3. Написать метод и вычислить факториал");
+            Console.Write("Введите число:");
+            bool IsNum = long.TryParse(Console.ReadLine(), out long Input);
+            bool IsFactor = Factor(ref Input);
+            if (IsNum)
+            {
+                if (IsFactor)
+                {
+                    Console.WriteLine($"Факториал числа - {Input}");
+                }
+                else
+                {
+                    Console.WriteLine("Факториал слишком большой");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число");
+            }
+        }
+        static void TASK4()
+        {
+            /*Упражнение 5.4 Написать рекурсивный метод вычисления факториала числа.*/
+            Console.WriteLine("Упражнение 5.4. Вычислить факториал с помощью рекурсии");
+            Console.Write("Введите число факториал которого хотите вычислить:");
+            bool IsNum = long.TryParse(Console.ReadLine(), out long Input);
+            if (IsNum)
+            {
+                long Factor = FactorRecursion(Input);
+                if (!(Factor == 0))
+                {
+                    Console.WriteLine($"Факториал заданного числа = {Factor}");
+                }
+
+            }
+
+        }
+        static void TASK5()
+        {
+            /*Домашнее задание 5.1 Написать метод, который вычисляет НОД двух натуральных чисел
+            (алгоритм Евклида). Написать метод с тем же именем, который вычисляет НОД трех
+            натуральных чисел.*/
+            Console.WriteLine("Домашнее задание 5.1. Создать метод, вычисляющий НОД двух и трех чисел");
+            Console.Write("Введите первое число:");
+            bool IsInt = int.TryParse(Console.ReadLine(), out int Int);
+            Console.Write("Введите второе число:");
+            bool IsInt2 = int.TryParse(Console.ReadLine(), out int Int2);
+            if (IsInt && IsInt2)
+            {
+                if (Int > Int2)
+                {
+                    Console.WriteLine($"НОД этих чисел - {NOD(Int, Int2)}");
+                }
+                else
+                {
+                    Console.WriteLine($"НОД этих чисел - {NOD(Int2, Int)}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Нужно было ввести число");
+            }
+            Console.Write("Введите первое число:");
+            bool IsInt3 = int.TryParse(Console.ReadLine(), out int Int3);
+            Console.Write("Введите второе число:");
+            bool IsInt4 = int.TryParse(Console.ReadLine(), out int Int4);
+            Console.Write("Введите третье число:");
+            bool IsInt5 = int.TryParse(Console.ReadLine(), out int Int5);
+            if (IsInt3 && IsInt4 && IsInt5)
+            {
+                int[] Ints_AR = { Int3, Int4, Int5 };
+                Array.Sort(Ints_AR);
+                Console.WriteLine($"НОД этих чисел - {NOD(Ints_AR[0], Ints_AR[1], Ints_AR[2])}");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число");
+            }
+        }
+        static void TASK6()
+        {
+            /*Домашнее задание 5.2 Написать рекурсивный метод, вычисляющий значение n-го числа
+            ряда Фибоначчи. Ряд Фибоначчи – последовательность натуральных чисел 1, 1, 2, 3, 5, 8,
+            13... Для таких чисел верно соотношение Fk = Fk-1 + Fk-2 .*/
+            Console.WriteLine("Домашнее задание 5.2. Создать рекурсивный метод, вычисляющий значение n-го числа ряда Фибоначчи");
+            Console.Write("Введите номер необходимого числа Фибоначчи:");
+            bool IsInt = int.TryParse(Console.ReadLine(), out int n);
+            if (IsInt)
+            {
+                Console.WriteLine($"Номер - {n}. Число - {Fib(n)}");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число");
+            }
+
+        }
+        static void Maximum(int a, int b)
+        {
+            Console.WriteLine($"Максимальное число - {Math.Max(a, b)}");
+        }
+        static void Swap(ref string str1, ref string str2)
+        {
+            string strSwap = str1;
+            str1 = str2;
+            str2 = strSwap;
+
+        }
+        static bool Factor(ref long a)
+        {
+            long res = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                try
+                {
+                    checked
+                    {
+                        res *= i;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    return false;
+                }
+            }
+            a = res;
+            return true;
+        }
+        static long FactorRecursion(long a)
+        {
+            try
+            {
+                checked
+                {
+                    if (a == 0)
+                    {
+                        return 1;
+                    }
+                    return a * FactorRecursion(a - 1);
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Факториал слишком большой");
+                return 0;
+            }
+        }
+        static int NOD(int a, int b)
+        {
+            int result = b;
+            while (!(a % b == 0))
+            {
+                result = a % b;
+                int Num = a;
+                a = b;
+                b = Num % b;
+            }
+
+            return result;
+        }
+        static int NOD(int a, int b, int c)
+        {
+            return NOD(NOD(a, b), c);
+        }
+        static int Fib(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+
+        }
+    }
+}
